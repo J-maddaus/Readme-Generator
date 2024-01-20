29f1f33198ebeca03b7ee1374a41258cb491f1a4
jacob maddaus <jakemaddaus@outlook.com>
2024-01-20 15:32:47 -0600

    added to the index.js file, and added a license.js file wiht links to license badges

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+// include needed packages for this application to run properly
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
+const licenseBadge = require('./licenseBadges').licenseBadge
+// An array of questions for the inquirer to ask and revice user input.
 inquirer
     .prompt([
         {
             name: 'license',
             message: 'Select what kind of license you would like for this application.',
             type: 'list',
-            choices: ["Academic Free License v3.0", "Apache license 2.0", "Artistic license 2.0", "Boost Software License 1.0", "BSD 2-clause license", "BSD 3-clause license", "Creative Commons Zero v1.0 Universal", "Creative Commons Attribution 4.0", "Creative Commons Attribution Share Alike 4.0", "Do What The Fuck You Want To Public License", "Educational Community License v2.0", "Eclipse Public License 1.0", "Eclipse Public License 2.0", "European Union Public License 1.1", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License v2.1", "GNU Lesser General Public License v3.0", "ISC", "LaTeX Project Public License v1.3c", "Microsoft Public License", "MIT", "Mozilla Public License 2.0", "Open Software License 3.0", "SIL Open Font License 1.1", "University of Illinois/NCSA Open Source License", "The Unlicense", "zLib License"]
+            choices: [
+                "GNU AGPLv3",
+                "GNU GPLv3",
+                "GNU LGPLv3",
+                "Mozilla",
+                "MIT",
+                "Apache",
+                "Boost",
+            ]
         },
         {
             name: 'Title',
@@ -40,9 +49,51 @@ inquirer
             message: 'How can i test this applicaton?',
             type: 'input'
         }
-
-
-
-
-
     ])
+    // genearates readme content based on user input
+    .then((responses) => {
+
+        const readmeContent = `
+ # ${responses.Title}
+
+## Table of Contents
+* [License](#license)
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [How to Contribute](#how-to-contribute)
+* [Tests](#tests)
+* [Questions?](#questions)
+
+## Description
+${responses.Description}
+
+## Installation
+${responses.Installation}
+
+## Usage
+${responses.Usage}
+
+## Contributions
+${responses.Contributions}
+
+## Testing
+${responses.Testing}
+
+## License
+${licenseBadge(responses.license)}
+Licensed under the [${responses.license}](LICENSE) license.
+`;
+
+        // Write README content to  new file
+        fs.writeFile('README.md', readmeContent, (err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('README.md successfully generated!');
+            }
+        });
+    });
+
+
+
